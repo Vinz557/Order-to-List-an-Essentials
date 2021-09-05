946089355f95614e5ec98dcb5e09d522951d886b
Vinz557 <71108814+Vinz557@users.noreply.github.com>
2021-09-05 09:05:48 +0800

    Update insert.php

--- 1st Project/insert.php
+++ 1st Project/insert.php
@@ -1,11 +1,41 @@
 <?php 
-     
-$name = $_POST['ownersName'];
-$products = $_POST['productName'];
-$quantity = $_POST['quantity'];
-$total = $_POST['price'];
+  
+	
+
+$conn  = mysqli_connect('localhost', 'root', '', 'orderlist');
+
+if (isset($_POST['submit'])) 
+{
 
 
+	if(!empty($_POST['ownersName']) && !empty($_POST['productName']) &&!empty($_POST['quantity']) &&!empty($_POST['price']))
+	{
+		
 
+		$name = $_POST['ownersName'];
+		$products = $_POST['productName'];
+		$quantity = $_POST['quantity'];
+		$price = $_POST['price'];
+
+
+		$query = "insert INTO orders(name,products,quantity,total) values ('$name','$products','$quantity','$price')";
+		$run = mysqli_query($conn,$query) or die(mysqli_error());
+
+		if($run)
+		{
+			echo "Form submitted successfully";
+		}else
+		{
+			echo "Form not submitted successfully";
+		}
+
+
+	}else
+	{
+		echo "all fields required";
+	}
+
+
+}
 
  ?>
